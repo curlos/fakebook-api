71a8f4da72618d2c1b351fd49ed75bbe5112a7f2
Carlos Martinez <curlosmart@gmail.com>
2021-10-23 11:28:57 -0400

    auth route setup

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,50 @@
 const router = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
+
+// register
+router.post('/register', async (req, res) => {
+  try {
+    // generate a new password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    // create a new user
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    })
+
+    // save user and respond
+    const user = await newUser.save()
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json({error: err})
+  }
+})
+
+
+// login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    
+    if (!user) {
+      res.status(404).json('user not found')
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+
+    if (!validPassword) {
+      res.status(400).json('wrong password')
+    }
+
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json({error: err})
+  }
+})
 
 module.exports = router;
